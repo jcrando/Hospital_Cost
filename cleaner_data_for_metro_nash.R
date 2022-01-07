208395659edbb783942f874e04bfc13b4d578f0c
jake <jcrando@gmail.com>
2022-01-06 23:16:33 -0500

    streamline data col

--- cleaner_data_for_metro_nash.R
+++ cleaner_data_for_metro_nash.R
@@ -1,43 +1,25 @@
 library(tidyverse)
 
+
 tn_hos_data= read_csv('data_science/hosptial-cost-/CSV_2018_Hospital_Cost_Report.csv') %>% 
   filter(`State Code`== 'TN')
 
-tn_metro_list = read_csv('data_science/hosptial-cost-/metro_list1_2020 - List 1.csv') %>% 
-  filter(`...3`==400) %>% 
-  select(-...2, -...6,) %>% 
+
+v1= c('CBSA Code','Metropolitan Division Code','CSA Code','CBSA Title','Metropolitan/Micropolitan Statistical Area','Metropolitan Division Title','CSA Title','County/County Equivalent','State Name','FIPS State Code','FIPS County Code','Central/Outlying County')
+raw_metro_list = read_csv('data_science/hosptial-cost-/metro_list1_2020 - List 1.csv')
+names(raw_metro_list)[1:12] <- v1
+raw_metro_list <- raw_metro_list[-1, ]
+raw_metro_list <- raw_metro_list[-1, ]
+
+
+tn_metro_list= raw_metro_list%>%   
+  filter(`CSA Code`==400) %>% 
+  select(-`Metropolitan Division Code`, -`Metropolitan Division Title`,) %>% 
   mutate_if(is.character,str_to_upper) 
-
-tn_metro_list$...8= str_replace_all(tn_metro_list$...8, ' COUNTY', '' )
-  
-merge(tn_hos_data, tn_metro_list, by.x = 'County', by.y = '...8') %>% 
-  view()
-
+tn_metro_list$`County/County Equivalent`= str_replace_all(tn_metro_list$`County/County Equivalent`, ' COUNTY', '' )
   
 
-tn_metro_list %>% 
-  mutate_if(is.character,str_to_upper) %>% 
-  view()
+metro_tn_hos_data=merge(tn_hos_data, tn_metro_list, by.x = 'County', by.y = 'County/County Equivalent') 
 
-metro_nash_hos_data= tn_hos_data %>% 
-  filter(City== 'NASHVILLE'| City=='MURFREESBORO'| City=='FRANKLIN'| City=='HENDERSONVILLE'| City=='SMYRNA'| City=='COLUMBIA'| City=='GALLATIN'| City=='HERMITAGE'| City=='LEBANON'| City=='DICKSON'| City=='SPRINGFIELD'| City=='ASHLAND CITY'| City=='CARTHAGE'| City=='HARTSVILLE'| City=='LAFAYETTE'| City=='WOODBURY'| City=='HICKMAN')
-
-repl
-
-
-str_replace_all(tn_metro_list, ' COUNTY', '' ) %>% 
-  view()
   
-str_replace_all(tn_metro_list, '[qwertyuioplaksjdhfgzmxncbv]', tou) %>% 
-  view()  
-
-
-merge(tn_hos_data,tn_metro_list, by= ) %>% 
-  view()
-
-
-metro_nash_hos_data= tn_hos_data %>% 
-  filter(City== 'NASHVILLE'| City=='MURFREESBORO'| City=='FRANKLIN'| City=='HENDERSONVILLE'| City=='SMYRNA'| City=='COLUMBIA'| City=='GALLATIN'| City=='HERMITAGE'| City=='LEBANON'| City=='DICKSON'| City=='SPRINGFIELD'| City=='ASHLAND CITY'| City=='CARTHAGE'| City=='HARTSVILLE'| City=='LAFAYETTE'| City=='WOODBURY'| City=='HICKMAN')
-
-metro_nash_hos_data  
 
