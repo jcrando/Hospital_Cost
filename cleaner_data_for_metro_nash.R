368ab64b8bcc1cb1507b0c3ed0eff89f12816df4
jake <jcrando@gmail.com>
2022-01-11 22:30:19 -0500

    data cleaning done

--- cleaner_data_for_metro_nash.R
+++ cleaner_data_for_metro_nash.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-
+install.packages("rsconnect")
 
 tn_hos_data= read_csv('data_science/hosptial-cost-/CSV_2018_Hospital_Cost_Report.csv') %>% 
   filter(`State Code`== 'TN')
@@ -21,5 +21,19 @@ tn_metro_list$`County/County Equivalent`= str_replace_all(tn_metro_list$`County/
 
 metro_tn_hos_data=merge(tn_hos_data, tn_metro_list, by.x = 'County', by.y = 'County/County Equivalent') 
 
-  
+lean_metro_data= metro_tn_hos_data %>% 
+  select(County, `Hospital Name`, `Street Address`, City, `Zip Code`, `Inpatient Total Charges`, `Outpatient Total Charges`, `Combined Outpatient + Inpatient Total Charges`, `Hospital Total Discharges (V + XVIII + XIX + Unknown) For  Adults & Peds`)
 
+final_df= read_csv('data_science/hosptial-cost-/final_df.csv')
+
+
+#attempt to use a groupby on MERGE to do calculations
+group_by_final= final_df %>% 
+  group_by(MERGE) 
+
+#maybe try summarize
+
+group_by_final= group_by_final %>% 
+  mutate((sum(TOTAL_SERVICES*AVG_MEDICARE_PAYMENT)/(sum(TOTAL_SERVICES)))) 
+
+write_rds(group_by_final, 'final_data.rds')
