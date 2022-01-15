dd306f3fa95b1e71238430d34bfe88bd45544bbf
jake <jcrando@gmail.com>
2022-01-15 15:22:59 -0500

    figure out how to populate

--- project.R
+++ project.R
@@ -5,9 +5,19 @@ library(leaflet)
 
 hospitals=read_rds('map_and_hospitals.rds')
 hospitals= relocate(hospitals, Latitude,.after = Longitude) 
+hospitals= hospitals %>% 
+  mutate(contentbox= paste(hospitals$`Hospital Name`, '</br>',  hospitals$Address, '</br>', hospitals$`(sum(TOTAL_SERVICES * AVG_MEDICARE_PAYMENT)/(sum(TOTAL_SERVICES)))`))
 
 
-area_list= c('County','City','Zip Code','Hospital Name')
+hospitalIcon= makeIcon(iconUrl = 'clipart-hospital-hospital-building-6.png', 
+                       iconWidth = 35, iconHeight = 35)#,
+                       #iconAnchorX = 22, iconAnchorY = 94)
+
+
+
+area_list= hospitals %>% 
+  select(County,City,`Zip Code`,`Hospital Name`)
+
 procedure= hospitals %>% 
               select(HCPCS_DESCRIPTION)
 
@@ -17,38 +27,38 @@ header= dashboardHeader()
 sidebar= dashboardSidebar(
   selectInput(
     inputId = 'area',
-    label= 'Area',
-    choices= area_list),
+    label= 'County, City, Zip, or Hospital',
+    choices= area_list,
+    selected= 'DAVIDSON'),
+  
     
- ## uiOutput('moreControls'),
+ ## uiOutput('secondary_drop_down'),
   
 selectInput(
     inputId = 'procedure',
     label= 'Procedure',
-    choices= procedure)
+    choices= procedure,
+    selected= 'URINALYSIS, MANUAL TEST')
   )
 
 
 
 body= dashboardBody(fluidPage(
   leafletOutput('mymap'),
-  p(),
-  actionButton('recal', 'New points')
+####  p(),
+#### actionButton('recal', 'New points')
 ))
 
 ui= dashboardPage(header, sidebar, body)
 
 
 
-#hospitalIcon= icons('clipart-hospital-hospital-building-6.png', 
-#                    iconWidth = 10, iconHeight = 15,
-#                    iconAnchorX = 22, iconAnchorY = 94)
-  
+
 
 server= function(input, output, session) {
-  output$value <- reactive({ input$select })
+ ##### output$value <- reactive({ input$select })
   
-  ##output$moreControls= renderUI({
+  ##output$secondary_drop_down= renderUI({
     ##tagList(
       ##selectInput(
         ##inputId= 'city',
@@ -59,18 +69,23 @@ server= function(input, output, session) {
 ##  })
   
 
-  points <- eventReactive(input$recalc, {
-    cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
-  }, ignoreNULL = FALSE)
+  ###output$value <- reactive({ input$select })###
+  ### points <- eventReactive(input$recalc, { ###
+  ###   cbind(rnorm(40) * 2 + 13, rnorm(40) + 48) ###
+  ###  }, ignoreNULL = FALSE)###
   
   output$mymap <- renderLeaflet({
     leaflet() %>%
-      addProviderTiles(providers$Stamen.TonerLite,
-                       options = providerTileOptions(noWrap = TRUE)
+      addTiles(group = 'Open Street Map'
       ) %>%
-      addMarkers(data = hospitals, 
-                 label = (hospitals$`Hospital Name`))
-                 #icon = hospitalIcon )
+      addMarkers(data = hospitals %>% 
+                   filter(County== input$area | 
+                          City==input$area |
+                          `Zip Code`== input$area |
+                            `Hospital Name`== input$area) %>% 
+                            filter(HCPCS_DESCRIPTION== input$procedure), 
+                     popup =  ~contentbox,
+                 icon = hospitalIcon)
   })
 }
 shiny:: shinyApp(ui, server)
