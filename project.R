1ec6fb0a501e0b41af8627eeb0d357f20a7349cb
jake <jcrando@gmail.com>
2022-01-15 13:33:29 -0500

    procedure drop down added

--- project.R
+++ project.R
@@ -7,8 +7,10 @@ hospitals=read_rds('map_and_hospitals.rds')
 hospitals= relocate(hospitals, Latitude,.after = Longitude) 
 
 
-area_list= hospitals %>% 
-  select(County,City,`Zip Code`,`Hospital Name`)
+area_list= c('County','City','Zip Code','Hospital Name')
+procedure= hospitals %>% 
+              select(HCPCS_DESCRIPTION)
+
 
 header= dashboardHeader()
 
@@ -16,15 +18,16 @@ sidebar= dashboardSidebar(
   selectInput(
     inputId = 'area',
     label= 'Area',
-    choices= area_list,
+    choices= area_list),
     
-    selectInput(
-      inputId= 'city',
-      label= 'City',
-      choices= c()
-    )
+ ## uiOutput('moreControls'),
+  
+selectInput(
+    inputId = 'procedure',
+    label= 'Procedure',
+    choices= procedure)
   )
-)
+
 
 
 body= dashboardBody(fluidPage(
@@ -35,10 +38,27 @@ body= dashboardBody(fluidPage(
 
 ui= dashboardPage(header, sidebar, body)
 
+
+
+#hospitalIcon= icons('clipart-hospital-hospital-building-6.png', 
+#                    iconWidth = 10, iconHeight = 15,
+#                    iconAnchorX = 22, iconAnchorY = 94)
   
 
 server= function(input, output, session) {
   output$value <- reactive({ input$select })
+  
+  ##output$moreControls= renderUI({
+    ##tagList(
+      ##selectInput(
+        ##inputId= 'city',
+        ##label= 'City',
+        ##choices= c()
+##     )
+##    )
+##  })
+  
+
   points <- eventReactive(input$recalc, {
     cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
   }, ignoreNULL = FALSE)
@@ -49,7 +69,8 @@ server= function(input, output, session) {
                        options = providerTileOptions(noWrap = TRUE)
       ) %>%
       addMarkers(data = hospitals, 
-                   label = (hospitals$`Hospital Name`))
+                 label = (hospitals$`Hospital Name`))
+                 #icon = hospitalIcon )
   })
 }
 shiny:: shinyApp(ui, server)
